c3682cd1545070afe393f937feb0ae2d81a30360
elliotjonesdev <elliotjonesdev@gmail.com>
2023-11-16 19:34:44 +0000

    set debug to false

--- thesoupfan/settings.py
+++ thesoupfan/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-soup-fan-6573d32fd512.herokuapp.com', 'localhost']
 
