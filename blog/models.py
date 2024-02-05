4d0094d00adce28424873a9e13294104ca7dbdf1
elliotjonesdev <elliotjonesdev@gmail.com>
2024-02-05 21:28:10 +0000

    add blog post functionality to django admin

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,13 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    publish_date = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey (User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     status = models.CharField(max_length=10, choices=options, default='draft')
+    
+    class Meta:
+        ordering = ('-publish',)
+    
+    def __str__(self):
+        return self.title
