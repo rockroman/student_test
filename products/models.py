34e5879ec737f116267bfbabaccad70e179e3031
elliotjonesdev <elliotjonesdev@gmail.com>
2024-03-01 21:22:14 +0000

    add newsletter app and subscibe text

--- products/models.py
+++ products/models.py
@@ -43,3 +43,4 @@ class Review(models.Model):
     content = models.TextField()
     created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
