f94ff19d34ba5d9589322d79481dfcec847f9fe6
elliotjonesdev <elliotjonesdev@gmail.com>
2024-03-07 21:15:02 +0000

    add edit and delete pages to blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='blog'),
+    path('comment/<int:pk>/edit/', views.edit_comment, name='edit_comment'),
+    path('comment/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
     path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from .forms import CommentForm
-from .models import Post
+from .models import Post, Comment
 
 def frontpage(request):
     posts = Post.objects.all()
-
     return render(request, 'blog/frontpage.html', {'posts': posts})
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
-
     if request.method == 'POST':
         form = CommentForm(request.POST)
-
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-
             return redirect('post_detail', slug=post.slug)
     else:
         form = CommentForm()
-
     return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
+
+@login_required
+def edit_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', slug=comment.post.slug)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'edit_comment.html', {'form': form})
+
+@login_required
+def delete_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('post_detail', slug=comment.post.slug)
+    return render(request, 'delete_comment.html', {'comment': comment})
